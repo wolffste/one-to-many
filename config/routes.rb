3c8b293bc04d94cd5cf0ad8d7646803ca1cdd42f
Rudi Boeke <r.boeke@me.com>
2020-03-03 17:53:42 +0100

    implement the datepickr

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :musicians, only: [ :index, :show ]
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bands do
+  resources :musicians do
     resources :bookings, only: [:new, :create]
   end
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bands
+
 
 end

80cbd2887ded2b5549de2afb3029c5beb7a10979
Stefan Wolff <wolffste@gmail.com>
2020-03-03 17:35:43 +0100

    messed

--- db/migrate/20200303133415_add_band_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class AddBandToGenres < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :genres, :band, foreign_key: true
-  end
-end
--- db/migrate/20200303142904_drop_table_genre.rb
+++
@@ -1,5 +0,0 @@
-class DropTableGenre < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :genres
-  end
-end

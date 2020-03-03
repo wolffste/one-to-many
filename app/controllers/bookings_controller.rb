7165e42f6c1cef154121a8d6ee2594186586260f
Rudi Boeke <r.boeke@me.com>
2020-03-03 12:57:30 +0100

    bookingnew

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,17 @@
 class BookingsController < ApplicationController
-  before_action :set_booking only: []
+  before_action :set_booking, only: []
 
   def new
-    @band = Band.find(params[:id])
+    @band = Band.find(params[:band_id])
     @booking = Booking.new
   end
 
   def create
-
+    @band = Band.find(params[:band_id])
+    @booking = Booking.new(booking_params)
+    @booking.band = @band
+    @booking.band.user = current_user
+    @booking.user = User.find(params[:user_id])
   end
 
 
@@ -15,6 +19,11 @@ class BookingsController < ApplicationController
 
   def set_booking
     @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:startdate, :enddate, :total_fee, :band_id)
+
   end
 
 end

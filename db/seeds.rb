9cd9a65d905f3671b2f1aa9f3c50968d786a97ca
Stefan Wolff <wolffste@gmail.com>
2020-03-03 17:05:29 +0100

    seed2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,70 @@
 require 'faker'
 
+User.destroy_all if Rails.env.development?
+Band.destroy_all if Rails.env.development?
+Genre.destroy_all if Rails.env.development?
 
+  user1 = User.new(
+    username: 'Rudi',
+    email: 'rudi@gmail.com',
+    password: '123456',
+    description: 'nice band',
+    fee: 100,
+    city: 'Jembke',
+    musician: true
+    )
+  user1.save!
+
+  user2 = User.new(
+    username: 'Stefan',
+    email: 'stefan@gmail.com',
+    password: '123456',
+    description: 'very nice band',
+    fee: 200,
+    city: 'Berlin',
+    musician: true
+    )
+  user2.save!
+
+  user3 = User.new(
+    username: 'Vera',
+    email: 'Vera@gmail.com',
+    password: '123456',
+    description: 'just a manager',
+    city: 'Berlin',
+    musician: false
+    )
+  user3.save!
+
+  3.times do
+    band = Band.new(
+      user_id: user1.id,
+      name: Faker::Hipster.word,
+      description: Faker::Hipster.sentence,
+      category: 'Rock'
+      )
+    band.save!
+  end
+
+  3.times do
+    band = Band.new(
+      user_id: user2.id,
+      name: Faker::Hipster.word,
+      description: Faker::Hipster.sentence,
+      category: 'Jazz'
+      )
+    band.save!
+  end
+
+  3.times do
+    band = Band.new(
+      user_id: user3.id,
+      name: Faker::Hipster.word,
+      description: Faker::Hipster.sentence,
+      category: 'Funk'
+      )
+    band.save!
+  end
 
   genres = ["Blues","Classic Rock","Country","Dance","Disco","Funk","Grunge",
  "Hip-Hop","Jazz","Metal","New Age","Oldies","Other","Pop","R&B",

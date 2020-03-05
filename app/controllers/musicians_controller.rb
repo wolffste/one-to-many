077cfbbc9d99d49af6c6fad22ef975eb81d077d2
Stefan Wolff <wolffste@gmail.com>
2020-03-05 18:05:15 +0100

    filter

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,8 +1,33 @@
 class MusiciansController < ApplicationController
 
+
+
   def index
-    @musicians = User.where(musician: true)
+    if params[:search].present? == false
+      @message = "Here are all our artists"
+      return @musicians = User.where(musician: true)
+    end
+
+
+
+    if params[:search][:instrument].present? && params[:search][:genre].present?
+      @musicians = User.where("instrument_id = ? and genre_id= ? ", "#{params[:search][:instrument]}", "#{params[:search][:genre]}")
+      @message = "Your search result"
+    elsif
+      params[:search][:instrument].present? && params[:search][:genre] == ""
+      @musicians = User.where("instrument_id = ? ", "#{params[:search][:instrument]}")
+      @message = "Your search result"
+    elsif
+      params[:search][:instrument] == "" && params[:search][:genre].present?
+      @musicians = User.where("genre_id = ? ", "#{params[:search][:genre]}")
+      @message = "Your search result"
+    else
+      @message = "Check out all our artists"
+      @musicians = User.where(musician: true)
+    end
   end
+
+
 
   def show
     @musicians = User.where(musician: true)
@@ -27,12 +52,12 @@ class MusiciansController < ApplicationController
   def bookmark
    @musician = @musicians.find(params[:id])
    @musician.bookmark(params[:musician_id])
- end
+  end
 
  private
 
- def musician_params
-  params.require(:user).permit(:description, :fee, :startdate, :enddate, :city, :birthday, :musician, :genre_id, :photo)
-end
+  def musician_params
+    params.require(:user).permit(:description, :fee, :startdate, :enddate, :city, :birthday, :musician, :genre_id, :photo)
+  end
 
 end
